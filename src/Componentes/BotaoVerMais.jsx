94e8b4269c03a8d01c8dd7f8082e63a6d2dff7a6
erikarodrigss <erikarodrigues@alu.ufc.br>
2026-05-21 20:42:32 -0300

    salvando para evitar conflito

---
+++ src/Componentes/BotaoVerMais.jsx
@@ -0,0 +1,11 @@
+import "../Styles/BotaoVerMais.css"
+
+function BotaoVerMais() {
+    return (
+        <div>
+            <button className="bnt-vermais">Ver mais</button>
+        </div>
+    )
+}
+
+export default BotaoVerMais
