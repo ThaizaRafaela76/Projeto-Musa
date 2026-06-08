3f5b86350f9708727024b83f0d7bca072aa3effb
ThaizaRafaela76 <thaizarafaela76@alu.ufc.br>
2026-06-08 14:54:27 -0300

    Organizando as pastas

---
+++ src/Pages/Artista/Artistas.jsx
@@ -0,0 +1,81 @@
+import Navbar from "../../Componentes/Navbar"
+import Rodape from "../../Componentes/Rodape";
+import PerfisArtistas from "../../Componentes/PerfisArtistas";
+import "../Styles/Artistas.css"
+
+
+const Artistas = () => {
+    const imagens = [
+        "src/assets/pagu.png",
+        "src/assets/auxiliadora.png",
+        "src/assets/djaniramotta.png",
+        "src/assets/hilda.png",
+        // "src/assets/lygia.png", 
+        // "src/assets/.png"
+    ];
+
+    const artistasMock = [
+        { id: 1, usuario: "Nina Sousa", tipo: "Pintura", cidade: "Quixadá" },
+        { id: 2, usuario: "Beatriz Soares", tipo: "Fotografia", cidade: "Mombaça" },
+        { id: 3, usuario: "Cristiane Menezes", tipo: "Escultura", cidade: "Choró" },
+        { id: 4, usuario: "Francisca da Silva", tipo: "Ilustração", cidade: "Pedra Branca" },
+        { id: 5, usuario: "Zeuda Honório", tipo: "Pintura", cidade: "Quixeramobim" },
+        { id: 6, usuario: "Leticia Queiroz", tipo: "Grafite", cidade: "Banabuiu" },
+        { id: 7, usuario: "Julia Laiza", tipo: "Fotografia", cidade: "Ibaretama" },
+        { id: 8, usuario: "Erika Rodrigues", tipo: "Escultura", cidade: "Milhã" },
+        { id: 9, usuario: "Larissa Maia", tipo: "Ilustração", cidade: "Senador" },
+        { id: 10, usuario: "Thaiza Rafaela", tipo: "Pintura", cidade: "Solonopole" },
+        { id: 11, usuario: "Martina Ribeiro", tipo: "Grafite", cidade: "Piquet Carneiro" },
+        { id: 12, usuario: "Aparecida Maria", tipo: "Fotografia", cidade: "Ibicuitinga" },
+    ];
+
+    const opcoesOrdem = [
+        { value: "az", label: "A → Z" },
+        { value: "za", label: "Z → A" },
+    ];
+
+    const opcoesFiltro = [
+        { value: "", label: "Todas as cidades" },
+        { value: "Banabuiu", label: "Banabuiu" },
+        { value: "Choró", label: "Choró" },
+        { value: "Deputado Irapuan Pinheiro", label: "Deputado Irapuan Pinheiro" },
+        { value: "Ibaretama", label: "Ibaretama" },
+        { value: "Ibicuitinga", label: "Ibicuitinga" },
+        { value: "Milhã", label: "Milhã" },
+        { value: "Mombaça", label: "Mombaça" },
+        { value: "Pedra Branca", label: "Pedra Branca" },
+        { value: "Piquet Carneiro", label: "Piquet Carneiro" },
+        { value: "Quixadá", label: "Quixadá" },
+        { value: "Quixeramobim", label: "Quixeramobim" },
+        { value: "Senador", label: "Senador" },
+        { value: "Solonopole", label: "Solonopole" },
+    ];
+
+
+
+    return (
+        <div>
+            
+            <div className="artistas">
+                <div className="navbar"><Navbar ></Navbar></div>
+                <div className="artistasConteudo">
+                    <div>
+                        <span className="text1">Artistas</span>
+                    </div>
+                    <div className="text2">
+                        <h1> São elas que criam. </h1>
+                    </div>
+                    <div className="imagem-artistas">
+                        <img
+                            src={"src/assets/Images.png"}
+                        />
+                    </div>
+                </div>
+            </div>
+            <PerfisArtistas mock={artistasMock} opcoesOrdem={opcoesOrdem} opcoesFiltro={opcoesFiltro}></PerfisArtistas>
+            <Rodape variante="bege" ></Rodape>
+        </div>
+    );
+}
+
+export default Artistas;
---
+++ src/Pages/Artista/Home.jsx
@@ -0,0 +1,181 @@
+import { useRef, useState } from "react"
+import { Link } from "react-router-dom"
+
+import "../Styles/Home.css"
+import Capa from "../assets/capahome.png"
+import { FaCircleArrowLeft } from "react-icons/fa6"
+import { FaCircleArrowRight } from "react-icons/fa6"
+import Navbar from "../../Componentes/Navbar"
+import mapa from "../assets/mapa.png"
+import Rodape from "../../Componentes/Rodape"
+import BotaoVerMais from "../../Componentes/BotaoVerMais"
+import CardTemplate from "../../Componentes/CardTemplate"
+import CardTemplateArtista from "../../Componentes/CardTemplateArtista"
+
+import Cisnei from '../assets/cisnei.png'
+import Tarde from '../assets/tardeamarela.png'
+import Gatos from '../assets/gatinhos.png'
+import Flor from '../assets/florecer.png'
+import Lirios from '../assets/lirios.png'
+import Lua from '../assets/lua.png'
+
+import perfil from '../assets/image 11.png'
+
+
+
+import imagemOcula from "../assets/ocula.png";
+import imagemTresorixas from "../assets/tresorixas.png";
+import imagemEusou from "../assets/eusou.png";
+import imagemAntropofagia from "../assets/antropofagia.png";
+import bannerAcervo from "../assets/banner-acervo.png";
+
+
+const Home = () => {
+    const ref = useRef(null)
+
+    function scrollDir() {
+        ref.current.scrollBy({ left: 300, behavior: 'smooth' })
+    }
+    function scrollEsq() {
+        ref.current.scrollBy({ left: -300, behavior: 'smooth' })
+    }
+
+    const refArtistas = useRef(null)
+    const [podeEsq, setPodeEsq] = useState(false)
+    const [podeDir, setPodeDir] = useState(true)
+
+    function verificaScroll() {
+        const el = refArtistas.current
+        setPodeEsq(el.scrollLeft > 0)
+        setPodeDir(el.scrollLeft + el.clientWidth < el.scrollWidth)
+    }
+
+    function scrollDirArtistas() {
+        refArtistas.current.scrollBy({ left: 300, behavior: 'smooth' })
+        setTimeout(verificaScroll, 350)
+    }
+    function scrollEsqArtistas() {
+        refArtistas.current.scrollBy({ left: -300, behavior: 'smooth' })
+        setTimeout(verificaScroll, 350)
+    }
+
+
+    return (
+        <div className="div-geral">
+            <header className="inicio-home">
+                <Navbar />
+                <div className="capa-home">
+                    <img src={Capa} />
+                </div>
+
+            </header>
+
+            <main className="corpo-home">
+                <section className="sobre-info" id="sobre">
+                    <div>
+                        <h1 className="sobre-titulo">Sobre nós</h1>
+                        <h2 className="sobre-texto">A Musa é um projeto de plataforma digital que oferece as artistas mulheres do Sertão Central
+                            um espaço para expor livremente seus trabalhos e perfis, além de proporcionar uma conexão entre
+                            artistas e público. </h2>
+                        <h2 className="sobre-texto2">O sistema busca reconhecer e dar visibilidade à importância dessas artistas na construção da
+                            identidade cultural da região, contribuindo para o fortalecimento do empoderamento feminino e
+                            para a construção de um cenário artístico mais inclusivo e representativo, ao mesmo tempo em que
+                            preserva a memória cultural local e valoriza suas trajetórias.</h2>
+                    </div>
+                </section>
+                <section className="cidade-section">
+                    <div className="cidades-home">
+                        <h1 className="cidade-titulo">Território</h1>
+                        <h2 className="cidade-texto">O projeto reúne artistas mulheres do Sertão Central, região formada por 13 municípios com
+                            forte diversidade cultural e artística.</h2>
+                    </div>
+                    <div className="cidade-mapa">
+                        <img src={mapa} alt="mapa de territorio do sertão central" />
+                    </div>
+                </section>
+                <section className="div-post">
+                    <div className="post-home">
+                        <h1 className="post-titulo">Últimas postagens</h1>
+                        <Link to="/acervo"><BotaoVerMais /></Link>
+                    </div>
+                    <div className="post-card" ref={ref}>
+                        <CardTemplate
+                            imagem={Cisnei}
+                            titulo="Reflexo do amor"
+                            subtitulo="Isis Martins"
+                        />
+
+                        <CardTemplate
+                            imagem={Tarde}
+                            titulo="Tarde de primavera"
+                            subtitulo="Cristina Sabino"
+                        />
+
+                        <CardTemplate
+                            imagem={Gatos}
+                            titulo="Manhã azul"
+                            subtitulo="Sophia Ferreira"
+                        />
+
+                        <CardTemplate
+                            imagem={Flor}
+                            titulo="Florescer"
+                            subtitulo="Elisa Gois"
+                        />
+
+                        <CardTemplate
+                            imagem={Lirios}
+                            titulo="Silêncio em rosa"
+                            subtitulo="Bianca Barroso"
+                        />
+
+                        <CardTemplate
+                            imagem={Lua}
+                            titulo="Lua lunar"
+                            subtitulo="Raiane Cavalcante"
+                        />
+                    </div>
+                    <div className="div-setas">   {/* ← adiciona isso de volta */}
+                        <button className="bnt-esq" onClick={scrollEsq}><FaCircleArrowLeft /></button>
+                        <button className="bnt-dir" onClick={scrollDir}><FaCircleArrowRight /></button>
+                    </div>
+                </section>
+
+                <section className="div-artista">
+                    <div className="artista-home">
+                        <h1 className="artista-titulo">Conheça as Artistas</h1>
+                        <Link to="/artistas"><BotaoVerMais /></Link>
+                    </div>
+
+                    <div style={{ width: "52px", flexShrink: 0 }}>
+                        {podeEsq && (
+                            <button className="bnt-esq-artista" onClick={scrollEsqArtistas}>
+                                <FaCircleArrowLeft />
+                            </button>
+                        )}
+                    </div>
+
+                    <div className="div-artista-carrossel">
+                        <div className="artista-card" ref={refArtistas}  onScroll={verificaScroll}>
+                            <CardTemplateArtista imagem={perfil} nome="Eudenia Sousa" />
+                            <CardTemplateArtista imagem={perfil} nome="Luiza Matias" />
+                            <CardTemplateArtista imagem={perfil} nome="Rebeca Freitas" />
+                            <CardTemplateArtista imagem={perfil} nome="Yasmim Morais" />
+                        </div>
+                    </div>
+
+                    <div style={{ width: "52px", flexShrink: 0 }}>
+                        {podeDir && (
+                            <button className="bnt-dir-artista" onClick={scrollDirArtistas}>
+                                <FaCircleArrowRight />
+                            </button>
+                        )}
+                    </div>
+                </section>
+            </main>
+            <Rodape />
+        </div>
+    )
+}
+
+export default Home
