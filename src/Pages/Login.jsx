eea77c80f8763be484cf2d13fe9bd58687e4645d
Julia Souzza <julialaizasouza@gmail.com>
2026-06-18 02:31:23 -0300

    logo no login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import FormLogin from "../Componentes/FormLogin";
 import BannerLogin from "../Componentes/BannerLogin";
 
 import imagemLogin from "../assets/login-image.png";
+import logo from "../assets/logo_musa2.png";
 
 import "../Styles/Login.css";
 
@@ -9,6 +10,14 @@ const Login = () => {
   return (
     <main className="login-page">
 
+      {/* LOGO TOPO (fora do card, como seu CSS pede) */}
+      <img
+        src={logo}
+        alt="Musa"
+        className="login-logo-topo"
+      />
+
+      {/* CARD PRINCIPAL (não mexe na estrutura interna) */}
       <div className="login-card">
 
         <FormLogin />
